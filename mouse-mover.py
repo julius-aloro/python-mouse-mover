411d82f1e5e3a5f80ee68fa05a483b1601eb8de2
julius.aloro <113492624+julius-aloro@users.noreply.github.com>
2024-04-07 10:34:57 +0800

    Update mouse-mover.py
    Added comments!

--- mouse-mover.py
+++ mouse-mover.py
@@ -7,10 +7,11 @@ import time
 import pyautogui
 
 def mouse_mover():
-    pyautogui.moveTo(341, 921, 3)
-    pyautogui.moveTo(1808, 897, 3)
-    pyautogui.moveTo(341, 921, 3)
+    pyautogui.moveTo(341, 921, 3)    # Moves the pointer to a starting point, duration of movement is 3 seconds (adjust accordingly)
+    pyautogui.moveTo(1808, 897, 3)   # Moving pointer ...
+    pyautogui.moveTo(341, 921, 3)    # Moves the pointer back to starting point, duration of movement is also 3 seconds
 
 while True:
+    # Adjust on how frequent the script runs (120 is in seconds)
     time.sleep(120)
     mouse_mover()
